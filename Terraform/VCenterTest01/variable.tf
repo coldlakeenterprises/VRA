3e224bfbfce6f2c460702cc9ee105d91dcbb52b5
coldlakeenterprises <98614669+coldlakeenterprises@users.noreply.github.com>
2022-02-17 16:35:36 -0500

    all

--- Terraform/VCenterTest01/variable.tf
+++ Terraform/VCenterTest01/variable.tf
@@ -1,20 +1,27 @@
-# Input variable: url
-variable "url" {
-  description = "The URL of the vRealize Automation environment either vRA 8.x or vRA Cloud"
-  type = string
-  default = "https://CLE-VRA01.coldlake.local"
-}
-
-# Input variable: refresh_token
 variable "refresh_token" {
-  description = "The refresh token to connect to the vRealize Automation environment"
-  type = string
-  default = ""
 }
 
-# Input variable: insecure
-variable "insecure" {
-  description = "Should SSL verification be skipped? true = skip ssl verification"
-  type = bool
-  default = "false"
+variable "url" {
+}
+
+variable "cloud_account" {
+}
+
+variable "region" {
+}
+
+variable "zone" {
+
+}
+
+variable "project" {
+
+}
+
+variable "image_name" {
+
+}
+
+variable "network_name" {
+
 }
---
+++ Terraform/VCenterTest02/main.tf
@@ -0,0 +1,37 @@
+provider vra {
+  url           = var.url
+  refresh_token = var.refresh_token
+  insecure      = true
+}
+
+data "vra_project" "this" {
+  name = var.project_name
+}
+
+data "vra_catalog_item" "this" {
+  name            = var.catalog_item_name
+  expand_versions = true
+}
+
+resource "vra_deployment" "this" {
+  name        = var.deployment_name
+  description = "terraform test deployment"
+
+  catalog_item_id      = data.vra_catalog_item.this.id
+  catalog_item_version = var.catalog_item_version
+  project_id           = data.vra_project.this.id
+
+  inputs = {
+    flavor    = "Lab-Small"
+    image     = "CentOS7"
+    count     = 1
+    password  = "VMware1!"
+    hostname1 = "Terraform-VM"
+  }
+
+  timeouts {
+    create = "60m"
+    delete = "60m"
+    update = "60m"
+  }
+}
