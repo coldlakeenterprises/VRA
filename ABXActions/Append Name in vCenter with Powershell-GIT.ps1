1306520622f28ef5bad2e0c6cc26436d803bfb1e
coldlakeenterprises <98614669+coldlakeenterprises@users.noreply.github.com>
2023-02-23 17:00:36 -0500

    Action Version: abx test

--- ABXActions/Append Name in vCenter with Powershell-GIT.ps1
+++ ABXActions/Append Name in vCenter with Powershell-GIT.ps1
@@ -5,10 +5,10 @@ $old_name = $inputs.resourceNames[0]
 $new_name = $inputs.customProperties.newname
 # Action inputs required for vCenter Server connectivity 
 $vc_URL = $inputs.vcURL
-$vc_USR = $context.getSecret($inputs.vcUSR)
-$vc_PWD = $context.getSecret($inputs.vcPWD)
+$vc_USR = $context.getSecret($inputs.vcUser)
+$vc_PWD = $context.getSecret($inputs.vcPassword)
 # Connect to vCenter Server using URL and User credentials
-Connect-VIServer -Server $vc_URL -User $vc_USR -Password $vc_PWD -force
+Connect-VIServer -Server $vc_URL -User $vcUser -Password $vc_PWD -force
 # Get $old_name vm from Vcenter
 $vm = Get-VM $old_name
 #notify 
